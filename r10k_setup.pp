acc250dcee8a13b3826081cd0f6972695b4662cb
Ryan Kennedy <rkennedy@2ndwatch.com>
2016-07-18 04:23:34 -0700

    added hiera to r10k

--- r10k_setup.pp
+++ r10k_setup.pp
@@ -7,6 +7,25 @@ class { 'r10k':
       'remote'  => 'https://github.com/ryno75/puppet_control.git'
       'basedir' => "${::settings::codedir}/environments",
       'prefix'  => false,
+    },
+    'hiera' => {
+      'remote'  => 'https://github.com/ryno75/puppet_hiera.git'
+      'basedir' => "${::settings::codedir}/hiera",
+      'prefix'  => false,
     }
   }
+}
+
+# Create symlinks for hiera (environment MUST be set in puppet.conf)
+file { '/etc/hiera.yaml':
+  ensure => link,
+  target => "${::settings::codedir}/hiera/${environment}/hiera.yaml"
+}
+file { "${::settings::codedir}/hiera.yaml":
+  ensure => link,
+  target => "${::settings::codedir}/hiera/${environment}/hiera.yaml"
+}
+file { "${::settings::confdir}/hiera.yaml":
+  ensure => link,
+  target => "${::settings::codedir}/hiera/${environment}/hiera.yaml"
 }
